0d213be069855e9affe2e0cce47fe28272abef67
Abin-Isaac <35003281+Abin-Isaac@users.noreply.github.com>
2021-11-15 21:23:18 -0800

    Add files via upload

---
+++ Integer_Variable.go
@@ -0,0 +1,16 @@
+package main 
+
+import "fmt"
+// var age int //Varaible declaration place 1st 
+
+var age int
+
+
+func main(){
+//Varaible declaration place 2nd 
+fmt.Println("My age is ", age)
+age = 1
+fmt.Println("My age is ", age)
+age = 2
+fmt.Println("My age is ", age)
+}
