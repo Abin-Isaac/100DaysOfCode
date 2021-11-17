349ad97387bb4f670b0836e8f4ec294423af307d
Abin-Isaac <35003281+Abin-Isaac@users.noreply.github.com>
2021-11-17 08:16:23 -0800

    Add files via upload

---
+++ 16Nov_Var_Init_value.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var age   = 'a' // variable declaration with initial value and no need to infer the type here explicitly
+	var age1 int = 23
+
+	fmt.Println("My age is", age, "and his age is",age1)
