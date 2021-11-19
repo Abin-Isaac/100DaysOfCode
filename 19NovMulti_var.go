42e0e768627a89e8f5b934951a014fa33b1e09ac
Abin-Isaac <35003281+Abin-Isaac@users.noreply.github.com>
2021-11-19 01:52:29 -0800

    Add files via upload

---
+++ 19NovMulti_var.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var width, height int = 100, 50 //declaring multiple variables
+	var 
+	(
+	a=10
+	b='c'
+	c=8.9
+	)
+
+	fmt.Println("width is", width, "height is", height)
+	fmt.Print(a,b,c)
+}
